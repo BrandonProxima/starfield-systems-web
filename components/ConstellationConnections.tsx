b160df56cbb75c7df011237aa38bac2113333b37
Brandon <brandon>
2025-08-22 00:48:05 -0500

    Fix TypeScript errors in buffer attributes for build

--- components/ConstellationConnections.tsx
+++ components/ConstellationConnections.tsx
@@ -180,12 +180,14 @@ export default function ConstellationConnections({ scrollData = { position: 0, v
           count={positions.length / 3}
           array={positions}
           itemSize={3}
+          args={[positions, 3]}
         />
         <bufferAttribute
           attach="attributes-color"
           count={colors.length / 3}
           array={colors}
           itemSize={3}
+          args={[colors, 3]}
         />
       </bufferGeometry>
       <shaderMaterial
--- components/ParticleTrails.tsx
+++ components/ParticleTrails.tsx
@@ -103,12 +103,14 @@ export default function ParticleTrails({ scrollData = { position: 0, velocity: 0
           count={positions.length / 3}
           array={positions}
           itemSize={3}
+          args={[positions, 3]}
         />
         <bufferAttribute
           attach="attributes-color"
           count={colors.length / 3}
           array={colors}
           itemSize={3}
+          args={[colors, 3]}
         />
       </bufferGeometry>
       <shaderMaterial
